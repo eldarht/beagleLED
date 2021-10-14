6e038010dd6c561968d79fad326b9523adda23e2
matsjor <mats88j@gmail.com>
2021-10-14 14:03:22 +0000

    update2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#define LED2_PATH "/sys/class/leds/beaglebone:green:usr2"
 
 int main(int argc, char* argv[]){
   std::cout << "Hello World";
+
+  std::fstream fs;
+  cout << "Starting LED flash" << endl;
+
+  while (1)
+  {
+
+	  fs.open(LED0_PATH "/trigger", std::fstream::out);
+	  fs << "timer";
+	  fs.close();
+	  fs.open(LED0_PATH "/delay_on", std::fstream::out);
+	  fs << "100";
+	  fs.close();
+	  fs.open(LED0_PATH "/delay_off", std::fstream::out);
+	  fs << "100";
+	  fs.close();
+  }
 }
