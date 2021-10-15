7e3ec17bf0a809be53e6391780131bdf3be57348
matsjor <mats88j@gmail.com>
2021-10-15 08:09:53 +0000

    led blinking and working

--- src/heartbeat.cpp
+++ src/heartbeat.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
-#define LED2_PATH "/sys/class/leds/beaglebone:green:usr2"
+#define GPIO_PATH "/sys/class/gpio/"
 
-
-int main(int argc, char* argv[]) 
+void heartbeat()
 {
+	using namespace std::this_thread;     // sleep_for, sleep_until
+	using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 
 	std::fstream fs;
 	cout << "Starting LED flash" << endl;
 
-	while (1) 
+	fs.open(GPIO_PATH "export", std::fstream::out);
+	fs << "53";
+	fs.close();
+	fs.open(GPIO_PATH "gpio53/direction", std::fstream::out);
+	fs << "out";
+	fs.close();
+
+	while (1)
 	{
-
-		fs.open(LED0_PATH "/trigger", std::fstream::out);
-		fs << "timer";
+		fs.open(GPIO_PATH "/gpio53/value", std::fstream::out);
+		fs << "1";
 		fs.close();
-		fs.open(LED0_PATH "/delay_on", std::fstream::out);
-		fs << "100";
+		sleep_for(100ms);
+		fs.open(GPIO_PATH "/gpio53/value", std::fstream::out);
+		fs << "0";
 		fs.close();
-		fs.open(LED0_PATH "/delay_off", std::fstream::out);
-		fs << "100";
-		fs.close();
+		sleep_for(100ms);
 	}
-
-
 }
