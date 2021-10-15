484f8040705d140c108418abfc2395706e1dff5b
matsjor <mats88j@gmail.com>
2021-10-15 06:56:49 +0000

    fixed something

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 #define LED2_PATH "/sys/class/leds/beaglebone:green:usr2"
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
   std::cout << "Hello World";
 
   std::fstream fs;
@@ -14,14 +15,13 @@ int main(int argc, char* argv[]){
 
   while (1)
   {
-
-	  fs.open(LED0_PATH "/trigger", std::fstream::out);
+	  fs.open(LED2_PATH "/trigger", std::fstream::out);
 	  fs << "timer";
 	  fs.close();
-	  fs.open(LED0_PATH "/delay_on", std::fstream::out);
+	  fs.open(LED2_PATH "/delay_on", std::fstream::out);
 	  fs << "100";
 	  fs.close();
-	  fs.open(LED0_PATH "/delay_off", std::fstream::out);
+	  fs.open(LED2_PATH "/delay_off", std::fstream::out);
 	  fs << "100";
 	  fs.close();
   }
