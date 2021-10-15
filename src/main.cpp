dbffa9ea7255c6173176f2be0814e07a9b193d77
matsjor <mats88j@gmail.com>
2021-10-15 07:56:26 +0000

    sleep fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
-#define LED2_PATH "/sys/class/leds/beaglebone:green:usr2"
+#define GPIO_PATH "/sys/class/gpio/"
 
 int main(int argc, char* argv[])
 {
   std::cout << "Hello World";
 
+  using namespace std::this_thread;     // sleep_for, sleep_until
+  using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
+  using std::chrono::system_clock;
+
   std::fstream fs;
   cout << "Starting LED flash" << endl;
 
+  fs.open(GPIO_PATH "export", std::fstream::out);
+  fs << "53";
+  fs.close();
+  fs.open(GPIO_PATH "gpio53/direction", std::fstream::out);
+  fs << "out";
+  fs.close();
+
   while (1)
   {
-	  fs.open(LED2_PATH "/trigger", std::fstream::out);
-	  fs << "timer";
+	  fs.open(GPIO_PATH "/gpio53/value", std::fstream::out);
+	  fs << "1";
 	  fs.close();
-	  fs.open(LED2_PATH "/delay_on", std::fstream::out);
-	  fs << "100";
+	  sleep_for(100ms);
+	  fs.open(GPIO_PATH "/gpio53/value", std::fstream::out);
+	  fs << "0";
 	  fs.close();
-	  fs.open(LED2_PATH "/delay_off", std::fstream::out);
-	  fs << "100";
-	  fs.close();
+	  sleep_for(100ms);
   }
 }
