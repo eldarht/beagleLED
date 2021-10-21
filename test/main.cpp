a0d5b86b3158827ad66c2c61f3919caecb1fc6b4
Eldar Hauge Torkelsen <eht@datarespons.no>
2021-10-21 11:15:46 +0200

    Changed order of includes to make it compile on windows

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch.hpp>
 #include "log_formatter.hpp"
+#include <catch2/catch.hpp>
 
 TEST_CASE( "log_formatter contains input string", "[logging]" ) {
   char log_buffer[50];
